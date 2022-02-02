786a7baede3d8db232a701b9182ca3ec1a1d3e89
Christopher <chrisa.bailey92@gmail.com>
2022-02-02 10:47:50 -0500

    adding 01_Variables to full solution/project

---
+++ 01_Variables/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("GIVE ME YOUR NAME.");
+        string firstName = Console.ReadLine();
+        Console.WriteLine(firstName);
+
+        Console.WriteLine("What is your favoite color");
+        string color = Console.ReadLine();
+
+        Console.WriteLine("You said your color is " + color);
+        }
+    }
+}
