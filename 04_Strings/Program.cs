126699e23654f050007dfc8d2d4ca4d7b10cab5a
Christopher <chrisa.bailey92@gmail.com>
2022-02-02 12:24:38 -0500

    adding 04_Strings to Fundamentals Solutions/Projects

---
+++ 04_Strings/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string first = "The cars we sell are";
+            string second = "BMW, Lexus, Mercedes and Tulsa.";
+
+            string concatenatedResult = first + "  " + second;
+
+            Console.WriteLine(concatenatedResult);
+
+            string firstName = "Jenn";
+            string lastName = "Williams";
+
+            string compositeResult = string.Format("Her name is {0} {1}", firstName, lastName);
+            
+            Console.WriteLine(compositeResult);
+            Console.WriteLine("Her name is {0} {1}", firstName, lastName);
+
+            string interpolationResult = $"Her name is still {firstName} {lastName}!";
+            Console.WriteLine(interpolationResult);
+            Console.WriteLine($"Her name is still {firstName} {lastName}!");
+    }
+}
