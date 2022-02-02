6b12da7046a2bc2d5263d75b8e4ba6ba8f6935a4
Christopher <chrisa.bailey92@gmail.com>
2022-02-02 12:06:21 -0500

    adding 03_Operators to Fundamental Solution/Project

---
+++ 03_Operators/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int additionResult = 5 + 2;
+            Console.WriteLine("5 + 2 = " + additionResult);
+
+            int subtractionResult = 5 - 2;
+            Console.WriteLine("5 - 2 = " + subtractionResult);
+
+            int multiplicationResult = 5 * 2;
+            Console.WriteLine("5 * 2 = " + multiplicationResult);
+
+            double divisionResult = 5d / 2d;
+            Console.WriteLine("5 / 2 = " + divisionResult);
+
+            Console.WriteLine(10 % 2);
+            Console.WriteLine(10 % 9);
+            Console.WriteLine(16 % 3);
+            Console.WriteLine(19 % 5);
+            
+        }
+    }
+}
