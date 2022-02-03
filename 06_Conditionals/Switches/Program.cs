1029c59c1d50448a3eb9333d16b6a9d8833a0a91
Christopher <chrisa.bailey92@gmail.com>
2022-02-03 11:19:34 -0500

    addin Switches to Fundamentals Solution/Project

---
+++ 06_Conditionals/Switches/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Switches
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please give us your name!");
+            string name = Console.ReadLine();
+            switch(name)
+            {
+                case "Chris":
+                Console.WriteLine("Nice to meet you!" + name);
+                break;
+                default:
+                break;
+            }
+
+
+            Console.WriteLine("How are you feeling today on a scale of 1-5?");
+            string feelingRating = Console.ReadLine();
+
+            switch(feelingRating)
+            {
+                case "5":
+                Console.WriteLine("That is great to hear");
+                break;
+                case "4":
+                Console.WriteLine("Good Stuff!");
+                break;
+                case "3":
+                Console.WriteLine("Hope things improve.");
+                break;
+                case "2":
+                Console.WriteLine("Oh no, Sorry to hear that!");
+                break;
+                case "1":
+                Console.WriteLine("Sorry, but there's always tomorrow.");
+                break;
+                default:
+                Console.WriteLine("Sorry, what you put in is not what we asked");
+            }
+        }
+    }
+}
