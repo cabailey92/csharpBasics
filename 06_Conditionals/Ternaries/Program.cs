2812efa848b90dd83f451597d62b3fa121dfcec2
Christopher <chrisa.bailey92@gmail.com>
2022-02-03 10:32:43 -0500

    adding Ternaries to Fundamentals Solution/Project

---
+++ 06_Conditionals/Ternaries/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Ternaries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your age:");
+            string response = System.Console.ReadLine();
+            int age = int.Parse(response);
+            
+            string output = age >= 18 ? "You can vote!!" : "You're to young to vote!";
+
+            Console.WriteLine(output);
+        }
+    }
+}
